a14da27d855cee0ca8bff84dc301953528fba993
Rob Wilson <roobert@gmail.com>
2020-03-29 19:39:12 +0100

    s/MULTITUDE_SERVICE_ACCOUNT_KEY/GOOGLE_APPLICATION_CREDENTIALS/

--- multitude/firestore/client.py
+++ multitude/firestore/client.py
@@ -15,16 +15,16 @@ class MultitudeClient:
     db: any = field(init=False)
 
     def __post_init__(self):
-        if environ.get("MULTITUDE_SERVICE_ACCOUNT_KEY"):
+        if environ.get("GOOGLE_APPLICATION_CREDENTIALS"):
             credentials = firebase_admin.credentials.Certificate(
-                environ["MULTITUDE_SERVICE_ACCOUNT_KEY"]
+                environ["GOOGLE_APPLICATION_CREDENTIALS"]
             )
             firebase_admin.initialize_app(credentials)
         elif environ.get("FIRESTORE_EMULATOR_HOST"):
             firebase_admin.initialize_app()
         else:
             raise CredentialsError(
-                "not set: MULTITUDE_SERVICE_ACCOUNT_KEY or FIRESTORE_EMULATOR_HOST"
+                "not set: GOOGLE_APPLICATION_CREDENTIALS or FIRESTORE_EMULATOR_HOST"
             )
 
         self.db = firestore.client()
